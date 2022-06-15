ad5878a125dfc7af88f62159a4015b36f34ed48b
Kostiantyn Kovalenko <kostiantyn.kovalenko@hpe.com>
2022-06-15 18:21:09 +0300

    Setup script for Docker

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+IMG_NAME=${DOCKER_REGISTRY}bluedata/web-notes
+IMG_VER=2.0
+
+if [ ! $(docker image list | grep "${IMG_NAME}" | grep "${IMG_VER}") ] ; then
+    docker build -t ${IMG_NAME}:${IMG_VER} .
+    
+    if ${DOCKER_REGISTRY} ; then
+        docker push ${IMG_NAME}:${IMG_VER}
+    fi
+fi   
+
+docker run -it -p 5000:5000 ${IMG_NAME}:${IMG_VER}
