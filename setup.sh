f1c4b8cfcaac07fdb34ebfb9e49c24adab7781c3
Kostiantyn Kovalenko <kostiantyn.kovalenko@hpe.com>
2022-06-21 18:15:56 +0300

    Added configscripts

--- setup.sh
+++ setup.sh
@@ -4,8 +4,12 @@ IMG_NAME=${DOCKER_REGISTRY}bluedata/web-notes
 IMG_VER=1.0
 PUSHIMAGE=${PUSHIMAGE:-false}
 
+tar -czvf configscripts/appconfig.tgz configscripts/appconfig > /dev/null
+
 docker build -t ${IMG_NAME}:${IMG_VER} .
 
 if [ $PUSHIMAGE = "true" ] ; then
     docker push ${IMG_NAME}:${IMG_VER}
 fi
+
+rm configscripts/appconfig.tgz
